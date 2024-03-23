d2291ea5f9417a9445d85cf0d44eff5320ccc289
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-03-23 15:25:08 -0500

    upgrade pickup

--- Assets/Scripts/Gun/Shoot Mode/ShootModePickup.cs
+++ Assets/Scripts/Gun/Shoot Mode/ShootModePickup.cs
@@ -13,9 +13,27 @@ using UnityEngine;
 public class ShootModePickup : MonoBehaviour
 {
     public ShootingMode ShootMode;
+    public AudioClip PickUpSound;
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        GunController gun = other.GetComponent<GunController>();
+
+        if (gun == null) return;
+
+        if(PickUpSound != null)
+            AudioSource.PlayClipAtPoint(PickUpSound, gun.transform.position);
+
+        gun.LoadShootingMode(ShootMode);
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        if(ShootMode == null)
+        {
+            Debug.LogWarning(gameObject.name + " doesnt have a shoot mode");
+            Destroy (gameObject);
+        }
     }
 }
