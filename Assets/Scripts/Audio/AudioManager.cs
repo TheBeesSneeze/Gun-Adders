2bc3e4d8893794b5e77c134103ae9d12f3006ecd
real-claire <clairenoto0@gmail.com>
2024-03-26 17:28:31 -0500

    AAAAHHHHHHHH

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -52,6 +52,7 @@ public class AudioManager : MonoBehaviour
                 s.source.clip = s.clip;
                 s.source.volume = g.groupVolume;
                 s.source.spatialBlend = g.groupSpatialBlend;
+                s.source.outputAudioMixerGroup = soundMixer;
             }
         }
     }
