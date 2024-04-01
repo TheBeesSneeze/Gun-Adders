4706d3cc511ab847cb80ae7cb658a9db5242d433
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-04-01 13:47:51 -0500

    fixed gun error

--- Assets/Scripts/BulletEffectTextScript.cs
+++ Assets/Scripts/BulletEffectTextScript.cs
@@ -10,7 +10,7 @@ public class BulletEffectTextScript : MonoBehaviour
     public TextMeshPro effect2;
     public void Update()
     {
-        effect1.text = "1: " + gun.bulletEffect1.UpgradeName;
-        effect2.text = "2: " + gun.bulletEffect2.UpgradeName;
+        if (gun.bulletEffect1 != null) effect1.text = "1: " + gun.bulletEffect1.UpgradeName;
+        if (gun.bulletEffect2 != null) effect2.text = "2: " + gun.bulletEffect2.UpgradeName;
     }
 }
