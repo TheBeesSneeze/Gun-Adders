f7711a58999d20a1d9362fd4d14d49e68ba67076
SkyBeal <thesnugpug1@gmail.com>
2024-04-02 21:45:42 -0500

    player damage ui and regen

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -14,17 +14,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(PlayerStats))]
 public class PlayerBehaviour : CharacterType
 {
     private PlayerStats stats;
-    [SerializeField] private GameObject damageCanvas;
-    [SerializeField] private float visualDamageSeconds = 0.5f;
+    [SerializeField] private Image redVignette;
+    private float secondsSinceLastTookDamage;
     // Start is called before the first frame update
     protected override void Start()
     {
         SetStats();
+    }
+
+    public void Update()
+    {
+        if (secondsSinceLastTookDamage >= stats.SecondsUntilHealing)
+        {
+            CurrentHealth += stats.HealthRegen * Time.deltaTime;
+            CurrentHealth = Mathf.Min(CurrentHealth, stats.DefaultHealth);
+        }
+        secondsSinceLastTookDamage += Time.deltaTime;
+        
+        float t = CurrentHealth / stats.DefaultHealth;
+        t = 1 - t;
+        redVignette.color = new Color(redVignette.color.r, redVignette.color.g, redVignette.color.b, t);
     }
 
     public void SetStats()
@@ -37,19 +52,12 @@ public class PlayerBehaviour : CharacterType
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
-
-        damageCanvas.SetActive(true);
-        StartCoroutine(TakeDamageVisual());
-    }
-
-    public IEnumerator TakeDamageVisual()
-    {
-        yield return new WaitForSeconds(visualDamageSeconds);
-        damageCanvas.SetActive(false);
+        secondsSinceLastTookDamage = 0;
     }
 
     public override void Die()
     {
-        SceneManager.LoadScene("Arena Scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-}
+    }
+
